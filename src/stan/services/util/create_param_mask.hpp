e04cf2bb4ceec78be448f9938e44b3cf7424f410
Ben <bbbales2@gmail.com>
2021-03-23 13:16:48 -0400

    mask tests working!

--- src/stan/services/util/create_param_mask.hpp
+++ src/stan/services/util/create_param_mask.hpp
@@ -6,40 +6,45 @@
 #include <rapidjson/stringbuffer.h>
 #include <rapidjson/writer.h>
 #include <string>
+#include <vector>
+#include <stan/math.hpp>
 
 namespace stan{
 namespace services{
 namespace util{
 
-void recurse_obj(std::vector<double>& v, const rapidjson::Value& r, bool clamped){
-  if (r["type"].GetString() == "array"){
-    for(int i = 0; i < r["length"].GetInt(); i++){
-      recurse_obj(v, r["type"].GetObject()["element_type"]);
+template<typename T>
+void recurse_obj(std::vector<double>& v, const T& rtype_obj, bool clamped){
+  std::string name = rtype_obj["name"].GetString();
+  if (name == "array") {
+    for(int i = 0; i < rtype_obj["length"].GetInt(); i++){
+      recurse_obj(v, rtype_obj["element_type"].GetObject(), clamped);
     }
   }
-  else if(r["type"].GetString() == "matrix"){
-    v.insert(v.end(), r["rows"].GetInt() * r["cols"].GetInt(), clamped ? 1 : 0);
+  else if(name == "matrix") {
+    v.insert(v.end(), rtype_obj["rows"].GetInt() * rtype_obj["cols"].GetInt(),
+	     clamped ? 1 : 0);
   }
-  else if(r["type"].GetString() == "vector"){
-    v.insert(v.end(), r["length"].GetInt(), clamped ? 1 : 0);
+  else if(name == "vector") {
+    v.insert(v.end(), rtype_obj["length"].GetInt(), clamped ? 1 : 0);
   }
   else{
     v.push_back(clamped? 1 : 0);
   }
 }
 
-Eigen::VectorXd create_param_mask(string& unconstrained_param_str, std::set<std::string>& clamped){
+Eigen::VectorXd create_param_mask(std::string& unconstrained_param_str, std::set<std::string>& clamped){
   rapidjson::Document d;
-  d.Parse(unconstrained_param_str);
+  d.Parse(unconstrained_param_str.c_str());
 
   std::vector<double> mask;
 
   for (auto const& p : d.GetArray()){
     if(clamped.find(p.GetObject()["name"].GetString()) != clamped.end()){
-      recurse_obj(mask, p.GetObject(), false);
+      recurse_obj(mask, p.GetObject()["type"].GetObject(), false);
     }
     else{
-      recurse_obj(mask, p.GetObject(), true);
+      recurse_obj(mask, p.GetObject()["type"].GetObject(), true);
     }
   }
   return Eigen::Map<Eigen::VectorXd>(mask.data(), mask.size());
--- src/test/unit/services/util/create_param_mask_test.cpp
+++ src/test/unit/services/util/create_param_mask_test.cpp
@@ -4,51 +4,51 @@
 #include <set>
 
 TEST(rng, create_param_mask_works) {
-  std::string ss;
-  ss << "[
-    {
-        \"name\": \"mat55\",
-        \"type\": {
-            \"name\": \"matrix\",
-            \"rows\": 5,
-            \"cols\": 5
-        },
-        \"block\": \"parameters\"
-    },
-    {
-        \"name\": \"arr5\",
-        \"type\": {
-            \"name\": \"array\",
-            \"length\": 5,
-            \"element_type\": {
-                \"name\": \"real\"
-            }
-        },
-        \"block\": \"parameters\"
-    },
-    {
-        \"name\": \"arr5mat\",
-        \"type\": {
-            \"name\": \"array\",
-            \"length\": 5,
-            \"element_type\": {
-                \"name\": \"matrix\",
-                \"rows\": 5,
-                \"cols\": 5
-            }
-        },
-        \"block\": \"parameters\"
-    },
-    {
-        \"name\": \"g\",
-        \"type\": {
-            \"name\": \"real\"
-        },
-        \"block\": \"generated_quantities\"
-    }
-]";
+  std::string ss = "["
+    "{"
+    "    \"name\": \"mat55\","
+    "    \"type\": {"
+    "        \"name\": \"matrix\","
+    "        \"rows\": 1,"
+    "        \"cols\": 2"
+    "    },"
+    "    \"block\": \"parameters\""
+    "},"
+    "{"
+    "    \"name\": \"arr5\","
+    "    \"type\": {"
+    "        \"name\": \"array\","
+    "        \"length\": 2,"
+    "        \"element_type\": {"
+    "            \"name\": \"real\""
+    "        }"
+    "    },"
+    "    \"block\": \"parameters\""
+    "},"
+    "{"
+    "    \"name\": \"arr5mat\","
+    "    \"type\": {"
+    "        \"name\": \"array\","
+    "        \"length\": 1,"
+    "        \"element_type\": {"
+    "            \"name\": \"matrix\","
+    "            \"rows\": 1,"
+    "            \"cols\": 1"
+    "        }"
+    "    },"
+    "    \"block\": \"parameters\""
+    "},"
+    "{"
+    "    \"name\": \"g\","
+    "    \"type\": {"
+    "        \"name\": \"real\""
+    "    },"
+    "    \"block\": \"generated_quantities\""
+    "}"
+    "]";
 
   std::set<std::string> clamped;
-  clamped.insert("mat55");
-  std::cout << stan::services::util::create_param_mask(ss, clampled) << std::endl;
+  //clamped.insert("mat55");
+  clamped.insert("arr5mat");
+  std::cout << stan::services::util::create_param_mask(ss, clamped) << std::endl;
 }
